75033949d8ff1a53f6b4b942b690c9a82b7b6169
Raj <r_patel46@u.pacific.edu>
2023-05-15 17:48:33 -0700

    delete the button from navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../styles/Navbar.css';
+
+
 
 function Navbar() {
   return (
-    <div className='navbar'>navbar
-      <div className='toggleButton'>
-        <button></button>
-       </div>
-       <div className='navlinks'>
+    <div className='navbar'>
+        {/*<div className='toggleButton'>*/}
+        {/*    <button>*/}
+        {/*        {''}*/}
+        {/*        <ReorderIcon />{''}*/}
+        {/*    </button>*/}
+        {/*</div>*/}
+        <div className='navlinks'>
         <Link to='/'>Home</Link>
         <Link to='/projects'>Projects</Link>
         <Link to='/experience'>Experience</Link>
-
-       </div>
+        </div>
     </div>
   )
 }
