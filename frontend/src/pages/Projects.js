73c07292a03c9e777bd56c947eec61278a2f4641
Raj <r_patel46@u.pacific.edu>
2023-06-21 23:55:45 -0700

    Added projects on project page with images

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import ProjectItem from "../components/ProjectItem";
+import img1 from '../images/speech_to_text.jpg';
+
+import '../styles/Projects.css';
 
  function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+     <h1>Academic Projects</h1>
+     <div className='projectList'>
+         <ProjectItem name='Speech to Text Summerization' image={img1}/>
+         <ProjectItem name='Speech to Sign Translation' image={img1}/>
+
+     </div>
+    </div>
   )
 }
 export default Projects;
